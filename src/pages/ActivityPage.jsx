6c113db75d895859883a1c70e22b6a2de448a666
Krunal <nsundersia@gmail.com>
2026-05-12 20:05:40 +0530

    Update ActivityPage.jsx

--- src/pages/ActivityPage.jsx
+++ src/pages/ActivityPage.jsx
@@ -1,4 +1,4 @@
-//hrhrhhhthht
+//hrhrhhhthh
 import React from 'react';
 import { useApp } from '../context/AppContext';
 import { Footprints, Timer, Moon, Play, Square, TrendingUp, Zap } from 'lucide-react';
