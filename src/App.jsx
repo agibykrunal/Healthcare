4d77c618d938a8ef3c7e7ef33d18c574c7ac9352
Krunal <nsundersia@gmail.com>
2026-05-26 20:30:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const PAGES = {
   chatbot:   ChatbotPage,
   register:  RegisterPage,
 };
-//fhfhfhgooososoosoooosooo
+//fhfhfhgooososoosoooosoo
 function Toast() {
   const { toast } = useApp();
   if (!toast) return null;
