b96803b31b7082fe88f41110fb8818bd6fa3e9e9
Krunal <nsundersia@gmail.com>
2026-06-01 12:07:28 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-/*hhhhhhhhhhhhhhhhhhhhhh*/
+/*hhhhhhhhhhhhhhhhhhhhh*/
 import { useApp } from '../context/AppContext';
 import {
   Footprints, Flame, Droplets, Heart, TrendingUp, TrendingDown,
