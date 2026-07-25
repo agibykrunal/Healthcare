7b9a2a7c5cbb220d859a51b8a4ee4cc44c362b61
Krunal <nsundersia@gmail.com>
2026-07-25 09:45:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';/*chhchchccccccccccccccccccccccc*/
+import './index.css';/*chhchchcccccccccccccccccccccc*/
 import { AppProvider, useApp } from './context/AppContext';
 import Sidebar from './components/Sidebar';
 import Topbar from './components/Topbar';
