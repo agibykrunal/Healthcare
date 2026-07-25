1c2bd67b568195e095072132993213080d78d7a7
Krunal <nsundersia@gmail.com>
2026-07-25 09:43:44 +0530

    Fix import statement formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './index.css';/*chhchchccccccccccccccccccccccccccccccccccc*./
 import { AppProvider, useApp } from './context/AppContext';
 import Sidebar from './components/Sidebar';
 import Topbar from './components/Topbar';
